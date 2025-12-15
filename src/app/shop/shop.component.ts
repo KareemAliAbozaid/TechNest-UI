442f437b0d0729edcae7c4773865ca3860305a34
Kareem Abozaid <karimabozaid487@gmail.com>
2025-12-15 22:51:59 +0200

    Add ngx-bootstrap integration and enhance shop component functionality
    - Integrated ngx-bootstrap for pagination in the shared module.
    - Updated shop component to support category filtering, sorting, and searching.
    - Modified shop service to handle new parameters for product retrieval.
    - Added ICategory model for category management.
    - Adjusted styles and HTML structure for improved user experience.

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/Models/product';
 import { IPagination } from '../shared/Models/pagination';
+import { ICategory } from '../shared/Models/category';
 
 @Component({
   selector: 'app-shop',
@@ -10,16 +11,65 @@ import { IPagination } from '../shared/Models/pagination';
 })
 export class ShopComponent implements OnInit {
   constructor(private readonly shopService: ShopService) { }
+
   products: IProduct[];
+  categories: ICategory[];
+  categoryId: number;
+  sortSelected: string;
+  search: string;
+
+  @ViewChild('search') searchInput: ElementRef;
+  @ViewChild('SortSelected') selected: ElementRef;
+
   ngOnInit(): void {
     this.getAllProducts();
+    this.getAllCategories();
   }
 
   getAllProducts() {
-    this.shopService.getProduct().subscribe({
+    this.shopService.getProduct(this.categoryId, this.sortSelected, this.search).subscribe({
       next: (value: IPagination) => {
         this.products = value.data;
       }
     })
+  }
+
+  getAllCategories() {
+    this.shopService.getCategories().subscribe({
+      next: (value) => {
+        this.categories = value;
+      }
+    })
+  }
+  selectedId(categoryId: number) {
+    this.categoryId = categoryId;
+    this.getAllProducts();
+  }
+
+  sortingOptions = [
+    { name: 'Default', value: 'name' },
+    { name: 'Price: Low to High', value: 'price_asc' },
+    { name: 'Price: High to Low', value: 'price_desc' },
+  ];
+
+
+  sortingByPrice(sort: Event) {
+    this.sortSelected = (sort.target as HTMLSelectElement).value;
+    console.log('sortSelected =>', this.sortSelected);
+    this.getAllProducts();
+  }
+
+  onSearch(search: string) {
+    this.search = search;
+    this.getAllProducts();
+  }
+
+  reSetValue() {
+    this.search = '';
+    this.sortSelected = this.sortingOptions[0].value;
+    this.categoryId = 0;
+    this.searchInput.nativeElement.value = '';
+    this.selected.nativeElement.selectedIndex = 0;
+    this.getAllProducts();
   }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagination } from '../shared/Models/pagination';
+import { ICategory } from '../shared/Models/category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,22 @@ export class ShopService {
 
   constructor(private readonly http: HttpClient) { }
   baseUrl = 'https://localhost:7185/api/';
-  getProduct() {
-    return this.http.get<IPagination>(this.baseUrl + 'Product');
+
+  getProduct(categoryId?: number, sortSelected?: string, search?: string) {
+    let param = new HttpParams();
+    if (categoryId) {
+      param = param.append('categoryId', categoryId.toString());
+    }
+    if (sortSelected) {
+      param = param.append('sort', sortSelected);
+    }
+    if (search) {
+      param = param.append('search', search);
+    }
+    return this.http.get<IPagination>(this.baseUrl + 'Product', { params: param });
+  }
+
+  getCategories() {
+    return this.http.get<ICategory[]>(this.baseUrl + 'Categories');
   }
 }
